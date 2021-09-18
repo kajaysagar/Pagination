11ec5196b45dfedf48857b7a7cfba4412c508e21
kajaysagar <31857707+kajaysagar@users.noreply.github.com>
2021-09-18 15:28:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const center = document.createElement("div");
+center.className = "center";
+
+center.innerHTML = `
+  <div class="pagination">
+ 
+    <a href="#">First</a>
+    <a href="#">Previous</a>
+    <a href="#">1</a>
+    <a href="#">2</a>
+    <a href="#">3</a>
+    <a href="#">4</a>
+    <a href="#" class="active">5</a>
+    <a href="#">6</a>
+    <a href="#">7</a>
+    <a href="#">8</a>
+    <a href="#">9</a>
+  </div>
+`;
+
+document.body.append(center);
